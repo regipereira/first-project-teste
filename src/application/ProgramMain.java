5094537fc132a5ad996f0398b0bca4a1e3418d3e
Reginaldo Silva <regipereira.silva@gmail.com>
2020-04-14 22:01:16 -0300

    Criada a classe ProgramMain

---
+++ src/application/ProgramMain.java
@@ -0,0 +1,12 @@
+package application;
+
+public class ProgramMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Olá Mundo!");
+		System.out.println("Primeiro Projeto no Git Hub");
+
+	}
+
+}
